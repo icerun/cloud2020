65ada829e46d6c40362f3f5be6745e5fb9ed426f
leak_shen <ice_run520@163.com>
2020-06-20 13:25:17 +0800

    pom修改

---
+++ cloud2020/cloudproviderpayment8004/src/main/java/com/myfunnel/springcloud/controller/PaymentController.java
@@ -0,0 +1,31 @@
+package com.myfunnel.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author shenshix
+ * @version 1.0
+ * @Date 2020/6/20 - 13:07
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+
+    /**
+     * 从配置文件中读取服务端口
+     */
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/zk/get")
+    public String paymentzk() {
+
+        return "Spring cloud with zk port:" + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+
+}
