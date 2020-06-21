cf1d80434a525af61a3a1b49fcad8bd8a4ce118d
leak_shen <ice_run520@163.com>
2020-06-21 08:59:52 +0800

    具体看代码

--- cloud2020/cloud-consumerconsul-order80/src/main/java/com/myfunnel/springcloud/OrderConsulMain80.java
+++ cloud2020/cloud-consumerconsul-order80/src/main/java/com/myfunnel/springcloud/OrderConsulMain80.java
@@ -1,9 +1,18 @@
 package com.myfunnel.springcloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * @author shenshix
  * @version 1.0
  * @Date 2020/6/20 - 17:59
  */
+@SpringBootApplication
+@EnableDiscoveryClient
 public class OrderConsulMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderConsulMain80.class,args);
+    }
 }
--- cloud2020/cloud-consumerconsul-order80/src/main/java/com/myfunnel/springcloud/controller/OrderConsulController.java
+++ cloud2020/cloud-consumerconsul-order80/src/main/java/com/myfunnel/springcloud/controller/OrderConsulController.java
@@ -1,9 +1,33 @@
 package com.myfunnel.springcloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
 /**
  * @author shenshix
  * @version 1.0
  * @Date 2020/6/20 - 18:00
  */
-public class PaymentController {
+@RestController
+@Slf4j
+public class OrderConsulController {
+
+    public static final String INVOKE_URL = "http://consul-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping(value = "/consumer/payment/consul")
+    public String getPaymentInfo() {
+
+        String resultInfo = restTemplate.getForObject(INVOKE_URL + "/payment/consul", String.class);
+        System.out.println("消费者调用支付服务提供者返回的结果：" + resultInfo);
+        return resultInfo;
+    }
+
+
 }
